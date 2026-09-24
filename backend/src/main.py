b033e6f406b65ef7c7f3e97985fe7cac12ee1fba
NexusAI <nexusai.aphile@gmail.com>
2026-09-24 20:04:40 +0200

    Fix backend syntax error in edge event handler

--- backend/src/main.py
+++ backend/src/main.py
@@ -320,7 +320,7 @@ async def edge_event(
 ):
     require_edge_token(x_nexusai_edge_token)
 
-    event_data = event.model_dump()\n    EDGE_EVENTS.insert(0, event_data)\n    del EDGE_EVENTS[200:]\n    dispatch_alert(event_data)\n
+    event_data = event.model_dump()    EDGE_EVENTS.insert(0, event_data)\n    del EDGE_EVENTS[200:]\n    dispatch_alert(event_data)
     site = EDGE_SITES.setdefault(event.site_id, {
         "site_id": event.site_id,
         "status": "ONLINE",
